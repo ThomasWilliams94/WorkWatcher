1201f1820651ed76961d974df6bd90972d0ddf9d
Thomas Williams <thomas.williams@live.co.uk>
2015-08-01 15:56:16 +0100

    Added new class: WorkWatcherData. This will contain all the information for the application.

---
+++ WorkWatcher/WorkWatcherData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWatcher
+{
+    /// <summary>
+    /// Contains all the data for the application, including details of 
+    /// Topics, Tasks, Days, etc.
+    /// </summary>
+    class WorkWatcherData
+    {
+        #region Members
+
+        /// <summary>
+        /// A list of all the topics that are defined in the application
+        /// </summary>
+        private List<Topic> itsTopics = new List<Topic>();
+
+        #endregion
+
+        #region Constructor
+
+        public WorkWatcherData();
+
+        #endregion
+
+    }
+}
