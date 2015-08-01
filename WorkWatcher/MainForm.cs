731e8169fcd85e9c3c050b19cac9279ff748844c
Thomas Williams <thomas.williams@live.co.uk>
2015-08-01 15:40:59 +0100

    Added a member to store a list of colours in preparation for topics, and added a method for adding colours to the list.

--- WorkWatcher/MainForm.cs
+++ WorkWatcher/MainForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace WorkWatcher
 {
@@ -19,6 +20,14 @@ namespace WorkWatcher
         /// </summary>
         private MainPanel itsMainPanel;
 
+        /// <summary>
+        /// Colours for the topics... hard code a few colours for now, 
+        /// until users can select the colour themselves. It's difficult
+        /// to get N distinct colours, and I don't have time to write a method
+        /// for doing that just yet. 
+        /// </summary>
+        private List<Color> itsColours = new List<Color>();
+
         #endregion
 
         #region Constructor
@@ -28,11 +37,33 @@ namespace WorkWatcher
         {
             InitializeComponent();
 
+            // Add colours to the list 
+            AddColoursToList();
+
             // Create the main panel and add it to the main form
             itsMainPanel = new MainPanel();
             itsMainPanel.Dock = DockStyle.Fill;            
             this.Controls.Add(itsMainPanel);
             itsMainPanel.BringToFront(); // This stops the panel from being covered by the tool and status strips
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Adds a list of colours to itsColours. Add more colours using this method
+        /// </summary>
+        private void AddColoursToList()
+        {
+            itsColours.Add(Color.Beige);
+            itsColours.Add(Color.White);
+            itsColours.Add(Color.MistyRose);
+            itsColours.Add(Color.GreenYellow);
+            itsColours.Add(Color.PaleVioletRed);
+            itsColours.Add(Color.PowderBlue);
+            itsColours.Add(Color.PeachPuff);
+
         }
 
         #endregion
