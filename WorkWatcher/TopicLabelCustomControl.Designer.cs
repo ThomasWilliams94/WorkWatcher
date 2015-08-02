550bbff1a6b5beed43f3502bb4648c60e33acffd
Thomas Williams <thomas.williams@live.co.uk>
2015-08-02 12:10:34 +0100

    Added new custom control: TopicLabelCustomControl. This has the context menu for editing/deleting topics.

---
+++ WorkWatcher/TopicLabelCustomControl.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace WorkWatcher
+{
+    partial class TopicLabelCustomControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.itsTopicLabel = new System.Windows.Forms.Label();
+            this.itsContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.itsContextMenuEditTopic = new System.Windows.Forms.ToolStripMenuItem();
+            this.itsContextMenuDeleteTopic = new System.Windows.Forms.ToolStripMenuItem();
+            this.itsContextMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // itsTopicLabel
+            // 
+            this.itsTopicLabel.AutoSize = true;
+            this.itsTopicLabel.ContextMenuStrip = this.itsContextMenu;
+            this.itsTopicLabel.Location = new System.Drawing.Point(0, 0);
+            this.itsTopicLabel.Name = "itsTopicLabel";
+            this.itsTopicLabel.Size = new System.Drawing.Size(100, 23);
+            this.itsTopicLabel.TabIndex = 0;
+            this.itsTopicLabel.Text = "Topic Custom Label";
+            // 
+            // itsContextMenu
+            // 
+            this.itsContextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.itsContextMenuEditTopic,
+            this.itsContextMenuDeleteTopic});
+            this.itsContextMenu.Name = "itsContextMenu";
+            this.itsContextMenu.Size = new System.Drawing.Size(141, 48);
+            // 
+            // itsContextMenuEditTopic
+            // 
+            this.itsContextMenuEditTopic.Name = "itsContextMenuEditTopic";
+            this.itsContextMenuEditTopic.Size = new System.Drawing.Size(140, 22);
+            this.itsContextMenuEditTopic.Text = "Edit Topic";
+            // 
+            // itsContextMenuDeleteTopic
+            // 
+            this.itsContextMenuDeleteTopic.Name = "itsContextMenuDeleteTopic";
+            this.itsContextMenuDeleteTopic.Size = new System.Drawing.Size(140, 22);
+            this.itsContextMenuDeleteTopic.Text = "Delete Topic";
+            this.itsContextMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label itsTopicLabel;
+        private System.Windows.Forms.ContextMenuStrip itsContextMenu;
+        private System.Windows.Forms.ToolStripMenuItem itsContextMenuEditTopic;
+        private System.Windows.Forms.ToolStripMenuItem itsContextMenuDeleteTopic;
+    }
+}
---
+++ WorkWatcher/TopicLabelCustomControl.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkWatcher
+{
+    public partial class TopicLabelCustomControl : Control
+    {
+        #region Constructor
+
+        public TopicLabelCustomControl()
+        {
+            InitializeComponent();
+
+            itsTopicLabel.AutoSize = false;
+            itsTopicLabel.Width = 100;
+
+            // Set up event handlers for context menu item clicks
+            itsContextMenuEditTopic.Click += new EventHandler(ItsContextMenuEditTopic_Click);
+            itsContextMenuDeleteTopic.Click += new EventHandler(ItsContextMenuDeleteTopic_Click);
+        }
+
+        #endregion
+
+        #region Properties
+
+        public Label TopicLabel
+        {
+            get
+            {
+                return itsTopicLabel;
+            }
+        }
+
+        #endregion
+
+        #region Override Methods
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        /// <summary>
+        /// Event handler for clicking the Edit context menu item. We display a 
+        /// topic form so the user can change details about the selected topic.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="me"></param>
+        private void ItsContextMenuEditTopic_Click(Object sender, EventArgs me)
+        {
+            // Show a message box for now while we restructure code. 
+            MessageBox.Show("Clicked edit item.");
+        }
+
+        /// <summary>
+        /// Event handler for clicking the Delete context menu item. We ask 
+        /// WorkWatcherData to delete the selected topic. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="me"></param>
+        private void ItsContextMenuDeleteTopic_Click(Object sender, EventArgs me)
+        {
+            // Show a message box for now while we restructure code.
+            MessageBox.Show("Clicked delete item.");
+        }
+
+        #endregion
+
+    }
+}
